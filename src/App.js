38494439c2c56fde86ae0fcb6592a22d2fe478e7
Dai Boyu <Dai.boyu@aub.edu.au>
2024-09-21 16:52:41 +1000

    Crate sotrage javascript

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import './App.scss'
 import Dashboard from './dashboard'
 
 function App() {
+  const db = new IndexedDB("TestDatabase","TestStore")
+  db.openDB().then(()=>{
+    console.log("DB sucessfully opened")
+  }).catch((error)=>{
+    console.error("Error while opening db")
+  })
   return (
     <div className='App'>
       <Dashboard />
