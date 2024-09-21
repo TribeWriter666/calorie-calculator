ac3a6c96c658b43592e3c001d1b6d1a6e418fb59
dahliasan <laninaesloco@gmail.com>
2024-09-22 05:59:01 +1000

    formatting

--- src/App.js
+++ src/App.js
@@ -3,50 +3,44 @@ import ConsumedFood, { nutritionTypes } from './ConsumedFood'
 import ConsumptionHistoryItem from './ConsumptionHistoryItem'
 import UploadImage from './UploadImage'
 import { useDB } from './DBProvider'
-import {useEffect, useState} from 'react'
+import { useEffect, useState } from 'react'
 import { upload } from '@testing-library/user-event/dist/upload'
 
-
 function App() {
-  const [uploading,setUploading] = useState(false)
-  const [responseList,setResponseList] = useState([])
-  const [dbError,setdbError] = useState(null)
+  const [uploading, setUploading] = useState(false)
+  const [responseList, setResponseList] = useState([])
+  const [dbError, setdbError] = useState(null)
   const db = useDB()
-  // process response list 
+  // process response list
 
-  let consumptionHistory = responseList.map((elem)=>{
+  let consumptionHistory = responseList.map((elem) => {
     let name = elem.food_name
     // remove the subscedenting g in macronutrient
-    let nutritions = Object.fromEntries(Object.entries(elem.macronutrients).map(([k, v]) => [k, parseInt(v)]));
-    return new ConsumedFood(
-      name,nutritions
+    let nutritions = Object.fromEntries(
+      Object.entries(elem.macronutrients).map(([k, v]) => [k, parseInt(v)])
     )
+    return new ConsumedFood(name, nutritions)
   })
 
+  useEffect(() => {
+    const fetch_data = async () => {
+      if (db) {
+        let list = await db.getAllItems()
 
-  useEffect(
-     ()=>{
-      const fetch_data = async () =>{    
-
-        if (db){
-          let list = await db.getAllItems()
-
-          setResponseList(list)
-          console.log("list setted")
-        }
+        setResponseList(list)
+        console.log('list setted')
       }
-      fetch_data()
     }
-  ,[db,uploading])
-
+    fetch_data()
+  }, [db, uploading])
 
   return (
     <div className='app'>
       <header>
-        <h1>Title TBD</h1>
+        <h1>How much did you eat today?</h1>
       </header>
       <div className='container'>
-        <UploadImage setUploading={setUploading}/>
+        <UploadImage setUploading={setUploading} />
 
         <h2 className='app-section-title'>Consumption History</h2>
         <ul className='list-group'>
